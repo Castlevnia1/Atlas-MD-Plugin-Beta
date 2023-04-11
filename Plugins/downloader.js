7086a594a1383a6f45bda1b903ca9923f4494696
Fantox <shubhapratimbiswas@gmail.com>
2023-04-11 21:03:36 +0530

    Update downloader.js

--- Plugins/downloader.js
+++ Plugins/downloader.js
@@ -1,10 +1,6 @@
 const axios = require("axios");
-const yts = require("youtube-yts");
-const googleit = require("google-it");
-const { ringtone } = require("../System/Scrapers");
-const { Sticker, StickerTypes } = require("wa-sticker-formatter");
-
-let mergedCommands = ["igdl", "mediafireDl"];
+const cheerio = require('cheerio');
+let mergedCommands = ["igdl", "instadl", "mediafiredl", "mediafire"];
 
 module.exports = {
   name: "mediaDownloader",
@@ -13,6 +9,7 @@ module.exports = {
   start: async (Atlas, m, { inputCMD, text, doReact, prefix, pushName }) => {
     switch (inputCMD) {
       case "igdl":
+      case "instadl":
         if (!text) {
           await doReact("❌");
           return reply(
@@ -47,8 +44,53 @@ module.exports = {
         );
         break;
 
+      case "mediafiredl":
+      case "mediafire":
+        if (!text) {
+          await doReact("❌");
+          return reply(
+            `Please provide a valid Mediafire link !\n\nExample: *${prefix}mediafire put_link*`
+          );
+        }
+        if (!text.includes("mediafire.com")) {
+          await doReact("❌");
+          return reply(
+            `Please provide a valid Mediafire link !\n\nExample: *${prefix}mediafire put_link*`
+          );
+        }
+
+        const MDF = await mediafireDl(text)
+        if (MDF[0].size.split('MB')[0] >= 100) return m.reply('File is too large in size!');
+
+        let txt = `        *『 Mediafire Downloader 』*
+        
+        *🎀 File Name* : ${MDF[0].nama}
+        *🧩 File Size* : ${MDF[0].size}
+        *📌File Format* : ${MDF[0].mime}
+        Downloading...`
+
+        await doReact("📥");
+        await reply(txt);
+
+        Atlas.sendMessage(m.from, { document: { url: MDF[0].url },mimetype: MDF[0].mime,fileName: MDF[0].nama,},{ quoted: m });
+        break;
+
       default:
         break;
     }
   },
 };
+
+async function mediafireDl(url) {
+  const res = await axios.get(url)
+  const $ = cheerio.load(res.data)
+  const results = []
+  const link = $('a#downloadButton').attr('href')
+  const size = $('a#downloadButton').text().replace('Download', '').replace('(', '').replace(')', '').replace('\n', '').replace('\n', '').replace('                         ', '')
+  const seplit = link.split('/')
+  const res5 = seplit[5]
+  resdl = res5.split('.')
+  resdl = resdl[1]
+  results.push({ res5, resdl, size, link })
+  return results
+}
