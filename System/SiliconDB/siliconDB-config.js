8d5f24c1e44b1592c0fb47a347ee928ba9785235
Ahmii <119128285+Ahmii-kun@users.noreply.github.com>
2023-04-27 00:09:57 +0500

    Update siliconDB-config.js

--- System/SiliconDB/siliconDB-config.js
+++ System/SiliconDB/siliconDB-config.js
@@ -1,100 +1,99 @@
 const axios = require("axios");
 
-async function banUser(userID) {
+const baseURL = "https://silicondb.32-pratyushprat.repl.co/api/data";
+
+async function updateUserBanStatus(userID, banStatus) {
   try {
-    const response = await axios.get(
-      `https://silicondb.32-pratyushprat.repl.co/api/data/${userID}`
-    );
-    if (response.status == 404 || !response.data) {
-      await axios.post("https://silicondb.32-pratyushprat.repl.co/api/data", {
-        id: userID,
-        ban: true,
-      });
-    }
-    if (response.data && response.data.ban !== true) {
-      await axios.put(
-        `https://silicondb.32-pratyushprat.repl.co/api/data/${userID}`,
-        {
-          id: userID,
-          ban: true,
-        }
-      );
-    } else {
-      await axios.put(
-        `https://silicondb.32-pratyushprat.repl.co/api/data/${userID}`,
-        {
-          id: userID,
-          ban: true,
-        }
-      );
-    }
-  } catch (err) {
-    //console.log(err);
-    await axios.post("https://silicondb.32-pratyushprat.repl.co/api/data", {
+    const response = await axios.get(`${baseURL}/${userID}`);
+    const userExists = response.status !== 404 && response.data;
+
+    const method = userExists ? axios.put : axios.post;
+    await method(`${baseURL}${userExists ? `/${userID}` : ""}`, {
       id: userID,
-      ban: true,
+      ban: banStatus,
+    });
+  } catch (err) {
+    await axios.post(baseURL, {
+      id: userID,
+      ban: banStatus,
     });
   }
 }
 
+const banUser = (userID) => updateUserBanStatus(userID, true);
+const unbanUser = (userID) => updateUserBanStatus(userID, false);
+
 async function checkBan(userID) {
   try {
-    const response = await axios.get(
-      `https://silicondb.32-pratyushprat.repl.co/api/data/${userID}`
-    );
-    if (
-      response.status == 404 ||
-      !response.data ||
-      response.data.ban !== true
-    ) {
-      return false;
-    } else {
-      return true;
-    }
+    const response = await axios.get(`${baseURL}/${userID}`);
+    return response.data && response.data.ban === true;
   } catch (err) {
     return false;
   }
 }
 
-async function unbanUser(userID) {
+async function updateModStatus(userID, modStatus) {
   try {
-    const response = await axios.get(
-      `https://silicondb.32-pratyushprat.repl.co/api/data/${userID}`
-    );
-    if (response.status == 404 || !response.data) {
-      await axios.post("https://silicondb.32-pratyushprat.repl.co/api/data", {
-        id: userID,
-        ban: false,
-      });
-    }
-    if (response.data && response.data.ban !== false) {
-      await axios.put(
-        `https://silicondb.32-pratyushprat.repl.co/api/data/${userID}`,
-        {
-          id: userID,
-          ban: false,
-        }
-      );
-    } else {
-      await axios.put(
-        `https://silicondb.32-pratyushprat.repl.co/api/data/${userID}`,
-        {
-          id: userID,
-          ban: false,
-        }
-      );
-    }
-  } catch (err) {
-    //console.log(err);
-    await axios.post("https://silicondb.32-pratyushprat.repl.co/api/data", {
+    const response = await axios.get(`${baseURL}/${userID}`);
+    const userExists = response.status !== 404 && response.data;
+
+    const method = userExists ? axios.put : axios.post;
+    await method(`${baseURL}${userExists ? `/${userID}` : ""}`, {
       id: userID,
-      ban: false,
+      mod: modStatus,
+    });
+  } catch (err) {
+    await axios.post(baseURL, {
+      id: userID,
+      mod: modStatus,
     });
   }
 }
+
+const setMod = (userID) => updateModStatus(userID, true);
+const removeMod = (userID) => updateModStatus(userID, false);
+
+async function checkMod(userID) {
+  try {
+    const response = await axios.get(`${baseURL}/${userID}`);
+    return response.data && response.data.mod === true;
+  } catch (err) {
+    return false;
+  }
+}
+
+async function fetchMods() {
+  try {
+    const response = await axios.get(`${baseURL}`);
+    const users = response.data;
+    const mods = users.filter(user => user.mod === true).map(user => user.id);
+    return mods;
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+}
+
+async function fetchBannedUsers() {
+  try {
+    const response = await axios.get(`${baseURL}`);
+    const users = response.data;
+    const bannedUsers = users.filter(user => user.ban === true).map(user => user.id);
+    return bannedUsers;
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+}
+
 
 module.exports = {
   banUser,
   checkBan,
   unbanUser,
+  setMod,
+  removeMod,
+  checkMod,
+  fetchMods,
+  fetchBannedUsers,
 };
