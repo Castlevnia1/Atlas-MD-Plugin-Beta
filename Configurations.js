1b5a738299e0a4f90d95aab95417ba0665a2a75e
Fantox <96835724+FantoX001@users.noreply.github.com>
2023-06-04 00:05:01 +0530

    Finalized

--- Configurations.js
+++ Configurations.js
@@ -7,17 +7,16 @@ if (!gg) {
 
 
 global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "mongodb+srv://tuff:tuff@cluster0.fhsvdnc.mongodb.net/test";
+global.mongodb = process.env.MONGODB || "Put your alternative mongodb url here";
 global.sessionId = process.env.SESSION_ID || "ok";
 global.prefa = process.env.PREFIX || "-";
 global.tenorApiKey = process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
 global.packname = process.env.PACKNAME || `Atlas Bot`;
 global.author = process.env.AUTHOR || "by: Team Atlas";
 global.port = process.env.PORT || "10000";
-global.openAiAPI = process.env.OPENAI_API || "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";
+global.openAiAPI = process.env.OPENAI_API || "Put your openai API key here";
 global.owner = gg.split(",");
 
 module.exports = {
   mongodb: global.mongodb,
-  useMongodb: global.useMongodb,
 };
