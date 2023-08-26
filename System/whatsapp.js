f9fe51e8147b39fbf4283c06457fddfda0731876
Fantox <96835724+FantoX001@users.noreply.github.com>
2023-08-26 15:04:08 +0530

    Update whatsapp.js

--- System/whatsapp.js
+++ System/whatsapp.js
@@ -8,7 +8,11 @@ const {
   delay,
   downloadContentFromMessage,
   getBinaryNodeChild,
+<<<<<<< HEAD
 } = require("baileysjs");
+=======
+} = require("../BaileysJS/lib");
+>>>>>>> 1cbf3fa642e55c58afe5b4f44b8b2f6b0771373c
 const fs = require("fs");
 const FileType = require("file-type");
 const { getRandom, fetchBuffer } = require("./Function");
